0ff3ff3120cc7c38c62edbf0ea6af28fbf370329
Kel Strother <kelstrother@gmail.com>
2023-04-19 12:08:38 -0600

    wrote functions for the search movie and tv functionality.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@
 //~ ///////////////////////////
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: "",
+    type: "",
+    page: 1,
+    totalPages: 1,
+  },
+  api: {
+    apiKey: "8103b0d834c98dad40a89daa11894775",
+    apiUrl: "https://api.themoviedb.org/3/",
+  },
 };
 
 //~ ///////////////////////////
@@ -210,9 +220,9 @@ async function displayShowDetails() {
             <li><span class="text-secondary">Episodes:</span> ${
               show.number_of_episodes
             }</li>
-            <li><span class="text-secondary">Next Episode Release:</span> ${
-              show.next_episode_to_air.air_date
-            }</li>
+             <li><span class="text-secondary">Next Episode Release:</span> ${
+               show.next_episode_to_air
+             }</li>
             <li><span class="text-secondary">Status:</span> ${show.status}</li>
           </ul>
           <h4>Production Companies</h4>
@@ -242,6 +252,22 @@ function displayBackgroundImage(type, backgroundPath) {
     document.querySelector("#movie-details").appendChild(overlayDiv);
   } else {
     document.querySelector("#show-details").appendChild(overlayDiv);
+  }
+}
+//?   SEARCH MOVIES/TV SHOWS
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  global.search.type = urlParams.get("type");
+  global.search.term = urlParams.get("search-term");
+
+  if (global.search.term !== '' && global.search.term !== null) {
+    //~ TODO - make request & display results
+    const results = await searchAPIData();
+    console.log(results);
+
+  } else {
+    showAlert('Please enter a search')
   }
 }
 
@@ -290,13 +316,30 @@ function initSwiper() {
 
 //? FETCH DATA FROM TMDB API
 async function fetchAPIData(endpoint) {
-  const API_KEY = "8103b0d834c98dad40a89daa11894775";
-  const API_URL = "https://api.themoviedb.org/3/";
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+  
+  showSpinner();
+  
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US&region=US`
+    );
+    const data = await response.json();
+    
+    hideSpinner();
+    
+    return data;
+  }
+  
+  //? MAKE REQUEST TO SEARCH
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US&region=US`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
   );
   const data = await response.json();
 
@@ -324,6 +367,15 @@ function highlightActiveLink() {
     }
   });
 }
+//? SHOW ALERT
+function showAlert(message, className) {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000)
+}
 
 //?   FUNCTION TO ADD COMMAS
 function addCommasToNumber(number) {
@@ -349,7 +401,7 @@ function init() {
       displayShowDetails();
       break;
     case "/search.html":
-      console.log("Search");
+      search();
       break;
   }
   highlightActiveLink();
